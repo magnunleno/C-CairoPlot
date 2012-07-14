bc6f72b10b3bad974870d403e7164703fc2a2f2c
Magnun Leno <magnun@codecommunity.org>
2012-07-14 01:37:41 -0300

    Basic Structs and Enums
    Added some basic structures and enumerations in util.h and updated the README.

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 C-CairoPlot
 ===========
 
-A reimplementation of CairoPlot using C
+A reimplementation of [CairoPlot](http://cairoplot.sourceforge.net/index.html)
+in C.
+
+**Attention:** This project is in very early development stage
+
+What is/was CairoPlot
+-----------------
+
+[CairoPlot is/was a pure Python plotting
+library](https://github.com/rodrigoaraujo01/cairoplot) which used Cairo C/Python API
+to create astonishing charts.
+
+It was recognized by it's great aesthetics, originally conceived by [Rodrigo
+Araujo](https://github.com/rodrigoaraujo01).
+
--- src/util.c
+++ src/util.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2012 - Magnun Leno da Silva
  * 
- * This file (util.h) is part of CairoPlot.
+ * This file (util.c) is part of CairoPlot.
  * 
  * CairoPlot is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free
@@ -18,31 +18,30 @@
  *
  */
 
-#ifndef _CP_UTIL_H_
-#define _CP_UTIL_H_
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "util.h"
 
-typedef enum CP_OBJ_TYPE
+char* CP_addFileExtension(char *filename, CP_FileType ft)
 {
-	CP_LIST_TYPE,
-	CP_CIRCULAR_LIST_TYPE,
-	CP_NODE_TYPE,
-	CP_DATA_TYPE,
-	CP_SERIE_TYPE,
-	CP_COLOR_TYPE,
-} CP_ObjectType;
-
-typedef enum CP_FILE_TYPE
-{
-	CP_SVG,
-	CP_PNG,
-} CP_FileType;
-
-typedef enum BOOL
-{
-	false = 0,
-	true = 1
-} bool;
-
-char* CP_addFileExtension(char *filename, CP_FileType ft);
-
-#endif // _CP_UTIL_H_
+	/* Adds an extension to a given filename
+	 *     @filename: String containing the filename;
+	 *     @ft: Enum which defines the extension;
+	 *     @return: pointer to the new allocated string (filenameExt);
+	 *
+	 */
+	char *filenameExt;
+	filenameExt = (char*)calloc(strlen(filename)+5, sizeof(char));
+	strcpy(filenameExt, filename);
+	switch(ft)
+	{
+		case CP_SVG:
+			strcat(filenameExt, ".svg");
+			break;
+		case CP_PNG:
+			strcat(filenameExt, ".png");
+			break;
+	}
+	return filenameExt;
+}
